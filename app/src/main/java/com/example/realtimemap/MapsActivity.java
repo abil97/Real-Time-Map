a8540d0b5eb30b9de6165c5941f58b0f9f9fb586
abil97 <abil.kuatbayev@nu.edu.kz>
2020-04-11 11:09:56 +0600

    Cleaned up the code

--- app/src/main/java/com/example/realtimemap/MapsActivity.java
+++ app/src/main/java/com/example/realtimemap/MapsActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -31,22 +32,24 @@ import java.util.Map;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private String url = "https://api.openweathermap.org/data/2.5/weather?q=Almaty&appid=42a4fd234571acd3ad844f8ffd4f74f7";  // Url fo get data from
-    private RestTemplate restTemplate;
-    private GetAWebResourceTask dataFetcher;
-    //public JSONObject CityWeather;
-    private static final String API_KEY = "42a4fd234571acd3ad844f8ffd4f74f7";
-    public String retunnumfromAsyncTask;
-
     private GoogleMap mMap;
+    private String url;                                      // Url fo get data from
+    private RestTemplate restTemplate;                      // Some stuff from SpringFramework for making Web Request
+    private GetAWebResourceTask dataFetcher;                // Some stuff for making async. task
+    LatLng Astana = new LatLng(51.18, 71.45);       // This is default location, where camera on map will be initially looking
+
+    private static final String API_KEY = "42a4fd234571acd3ad844f8ffd4f74f7";   // API Key is necessary for making a request.
+
+
+    // The HashMap of cities with coordinates
     private HashMap<String, HashMap<String, Double>> cities = new HashMap<String, HashMap<String, Double>>() {{
         put("Almaty", new HashMap<String, Double>(){{ put("lat", 43.25); put("lon", 76.94); }});
-//        put("Nur-Sultan", new HashMap<String, Double>(){{ put("lat", 51.18); put("lon", 71.45); }});
-//        put("Shymkent", new HashMap<String, Double>(){{ put("lat", 42.29); put("lon", 69.59); }});
-//        put("Karagandy", new HashMap<String, Double>(){{ put("lat", 49.50); put("lon", 73.10); }});
-//        put("Kostanay", new HashMap<String, Double>(){{ put("lat", 53.17); put("lon", 63.58); }});
-//        put("Atyrau", new HashMap<String, Double>(){{ put("lat", 47.12); put("lon", 51.88); }});
-//        put("Semey", new HashMap<String, Double>(){{ put("lat", 50.41); put("lon", 80.23); }});
+        put("Nur-Sultan", new HashMap<String, Double>(){{ put("lat", 51.18); put("lon", 71.45); }});
+        put("Shymkent", new HashMap<String, Double>(){{ put("lat", 42.29); put("lon", 69.59); }});
+        put("Karagandy", new HashMap<String, Double>(){{ put("lat", 49.50); put("lon", 73.10); }});
+        put("Kostanay", new HashMap<String, Double>(){{ put("lat", 53.17); put("lon", 63.58); }});
+        put("Atyrau", new HashMap<String, Double>(){{ put("lat", 47.12); put("lon", 51.88); }});
+        put("Semey", new HashMap<String, Double>(){{ put("lat", 50.41); put("lon", 80.23); }});
 
     }};
 
@@ -59,38 +62,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        Log.e("MAP of THE CITIES", cities.toString());
 
     }
-
-
-//    public String getWeatherType(String city){
-//        ArrayList<Map<String, Object>> weather = null;
-//        Map<String, Object> weather_0 = null;
-//        weather = (ArrayList<Map<String, Object>>) getCityWeather().get("weather");
-//        weather_0 = weather.get(0);
-//
-//        return (String) weather_0.get("main");
-//    }
-//
-//    public String getWeatherDescription(String city){
-//
-//        ArrayList<Map<String, Object>> weather = null;
-//        Map<String, Object> weather_0 = null;
-//        weather = (ArrayList<Map<String, Object>>) getCityWeather().get("weather");
-//        weather_0 = weather.get(0);
-//
-//        return (String) weather_0.get("description");
-//    }
-//
-//    public Double getTemp(String city){
-//        ArrayList<Map<String, Object>> weather = null;
-//        Map<String, Double> main = null;
-//        weather = (ArrayList<Map<String, Object>>) getCityWeather().get("weather");
-//        main = (Map<String, Double>) getCityWeather().get("main");
-//
-//        return main.get("temp");
-//    }
 
 
     /**
@@ -104,92 +77,77 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
-        // Set up RestTemplate() for making Web request
-        restTemplate = new RestTemplate();
+        mMap = googleMap;           // Initialize google map
 
-        // Set up a new object for asynchronous task
-        new GetAWebResourceTask(restTemplate, mMap).execute(url);
+        /* Iterating through map of cities defined above, and making an Web API request for each city*/
+        for (HashMap.Entry<String, HashMap<String, Double>> entry : cities.entrySet()) {
+
+            String currCity = entry.getKey();   // Get current city name
+            setUrl(currCity);                   // Set up the url for this specific city, for making API request
 
 
+            restTemplate = new RestTemplate();                                      // Set up RestTemplate() for making Web request
+            new GetAWebResourceTask(restTemplate, mMap, currCity).execute(url);     // Set up a new object for asynchronous task. Making a Web API Request
+        }
 
-//        for (HashMap.Entry<String, HashMap<String, Double>> entry : cities.entrySet()) {
-//            String currCity = entry.getKey();
-//            HashMap<String, Double> coords = entry.getValue();
-//            LatLng MapCoords = new LatLng(coords.get("lat"), coords.get("lon"));
-//
-//            setUrl(currCity);
-//            Log.e("CURRENT URL", getUrl());
-//            getData(dataFetcher, getUrl());
-//            Log.e("CURRENT CITYWEATHER", getCityWeather().toString());
-//            String weatherType = getWeatherType(currCity);
-//            Double temp = getTemp(currCity);
-//            String result = currCity + ":\n" + weatherType + ", " + temp;
-//
-//
-//            mMap.addMarker(new MarkerOptions().position(MapCoords).icon(BitmapDescriptorFactory
-//                    .defaultMarker(BitmapDescriptorFactory.HUE_GREEN)).title(result));
-//        }
+        // Initially move camera to Astana, with Landmass/continent view
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Astana));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(5));
 
-        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(-34, 151);
-//        mMap.addMarker(new MarkerOptions().position(sydney).icon(BitmapDescriptorFactory
-//                .defaultMarker(BitmapDescriptorFactory.HUE_GREEN)).title("Marker in Sydney"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
-
+    // Get the URL for web API request
     public String getUrl() {
         return url;
     }
 
+    // SET the URL for web API request, for specific city
     public void setUrl(String city) {
         this.url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + API_KEY;
     }
 
 
-
+    // All Asynchronous stuff is happening here
     private class GetAWebResourceTask extends AsyncTask<String, Void, String> {
 
-        final public static String TAG = "GetAWebResourceTask";
-        private RestTemplate restTemplate ;
-        private GoogleMap mMap;
-        public JSONObject CityWeather;
+        private RestTemplate restTemplate;          // Spring Framework stuff for making web request
+        private GoogleMap mMap;                     // Map
+        private String currentCity;                 // Name of the currently fetched city
+        public JSONObject CityWeather;              // Fetched city data in JSON format
 
 
         // Constructor
-        public GetAWebResourceTask(RestTemplate restTemplate, GoogleMap gmap) {
+        public GetAWebResourceTask(RestTemplate restTemplate, GoogleMap gmap, String city) {
             this.restTemplate = restTemplate ;
             this.mMap = gmap;
+            this.currentCity = city;
         }
 
+        // Json parser. Returns weather type - sunny, rain, cloud, etc.
         public String getWeatherType(String city){
-            ArrayList<Map<String, Object>> weather = null;
-            Map<String, Object> weather_0 = null;
-            weather = (ArrayList<Map<String, Object>>) CityWeather.get("weather");
-            weather_0 = weather.get(0);
+            ArrayList<Map<String, Object>> weather = (ArrayList<Map<String, Object>>) CityWeather.get("weather");;
+            Map<String, Object> weather_0 = weather.get(0);
 
             return (String) weather_0.get("main");
         }
 
+        // Json parser. Returns a description of a weather
         public String getWeatherDescription(String city){
 
-            ArrayList<Map<String, Object>> weather = null;
-            Map<String, Object> weather_0 = null;
-            weather = (ArrayList<Map<String, Object>>) CityWeather.get("weather");
-            weather_0 = weather.get(0);
+            ArrayList<Map<String, Object>> weather = (ArrayList<Map<String, Object>>) CityWeather.get("weather");
+            Map<String, Object> weather_0 =  weather.get(0);
 
             return (String) weather_0.get("description");
         }
 
+        // Json parser. Returns temperature in a given city
         public Double getTemp(String city){
-            ArrayList<Map<String, Object>> weather = null;
-            Map<String, Double> main = null;
-            weather = (ArrayList<Map<String, Object>>) CityWeather.get("weather");
-            main = (Map<String, Double>) CityWeather.get("main");
+            ArrayList<Map<String, Object>> weather = (ArrayList<Map<String, Object>>) CityWeather.get("weather");
+            Map<String, Double> main = (Map<String, Double>) CityWeather.get("main");
 
-            return main.get("temp");
+            // return temp. in Celsius
+            return main.get("temp") - 273.15;
         }
 
 
@@ -210,34 +168,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         @Override
         protected void onPostExecute(String result) {
 
-
-            JSONParser parser = new JSONParser();
+            JSONParser parser = new JSONParser();                       // Set up JSON Parser
             try {
-                CityWeather = (JSONObject) parser.parse(result);
-
-
+                CityWeather = (JSONObject) parser.parse(result);        // Parse result (String) into JSON Object
                 super.onPostExecute(result);
-
-
             } catch (ParseException e) {
                 e.printStackTrace();
             }
-            for (HashMap.Entry<String, HashMap<String, Double>> entry : cities.entrySet()) {
-                String currCity = entry.getKey();
-                HashMap<String, Double> coords = entry.getValue();
-                LatLng MapCoords = new LatLng(coords.get("lat"), coords.get("lon"));
+
+                HashMap<String, Double> coords = cities.get(currentCity);               // Get coordinates of current city
+                LatLng MapCoords = new LatLng(coords.get("lat"), coords.get("lon"));    // Initialize a new Google map LatLng object with coords
 
 
-                String weatherType = getWeatherType(currCity);
-                Double temp = getTemp(currCity);
-                String res = currCity + ":\n" + weatherType + ", " + temp;
+                String weatherType = getWeatherType(currentCity);                           // Get weather type
+                Double temp = getTemp(currentCity);                                         // Get temperature and round up to 1 decimal place
+                String roundedTemp = String.format("%.1f", temp);
+                String res = currentCity + "\n" + weatherType + ", " + roundedTemp + "°";   // Final String of data (weather type, temp) to be displayed on map
 
+
+                // Generate a new GoogleMAp icon with Android Utility Library
+                // https://developers.google.com/maps/documentation/android-sdk/utility
                 IconGenerator micongen = new IconGenerator(MapsActivity.this);
                 Bitmap mBitmap = micongen.makeIcon(res);
 
+                // Add marker with data on the map
                 mMap.addMarker(new MarkerOptions().position(MapCoords).icon(BitmapDescriptorFactory
-                        .fromBitmap(mBitmap)).title(currCity));
-        }
+                        .fromBitmap(mBitmap)).title(currentCity));
+
+
 
         }
     }
